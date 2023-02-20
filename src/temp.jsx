21bac96bb442e1ea03f47633a4b70571e9336fb4
katta chaithanya kumar <80614118+KattachaithanyaKumar@users.noreply.github.com>
2023-02-20 21:21:16 +0530

    initial commit

---
+++ src/temp.jsx
@@ -0,0 +1,25 @@
+<div>
+  <h1>7-Day Weather Forecast</h1>
+  <form onSubmit={handleSubmit}>
+    <input
+      type="text"
+      value={city}
+      onChange={(e) => setCity(e.target.value)}
+      placeholder="Enter city name"
+    />
+    <button type="submit">Search</button>
+  </form>
+  <div style={{ display: "flex" }}>
+    {forecast.map((item) => (
+      <div key={item.dateTimeISO}>
+        <img src={`https://openweathermap.org/img/wn/${item.icon}`} alt="" />
+        <h3>{new Date(item.dateTimeISO).toLocaleDateString()}</h3>
+        <p>
+          Temperature: {item.maxTempC}°C - {item.minTempC}°C
+        </p>
+        {/* <p>Description: {item.weather.replace("Mostly", "")}</p> */}
+        <p>Description: {item.weather}</p>
+      </div>
+    ))}
+  </div>
+</div>;
